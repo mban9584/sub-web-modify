be95254e5487fbd57d70476f713eecabad1d8d92
mban9584 <mban9584@icloud.com>
2025-10-03 02:09:10 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,6 +348,7 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
+          { value: "https://subapi.52088088.xyz" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
@@ -1311,4 +1312,5 @@ export default {
   }
 };
 </script>
+
 
